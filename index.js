3528adea25ba7421531074f892b2ae39a48bae41
Manmohandeep Singh <ltsbcsgo@gmail.com>
2023-01-13 09:40:00 -0500

    commit tree a8e8439b98250d7c288708016794fbe8e60ad51b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+document.addEventListener("keydown", function (event) {
+const dodger = document.getElementById("dodger");
+
+ if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+if (e.key === "ArrowRight") {
+    moveDodgerRight();
+}
+
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
